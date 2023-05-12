ab704f968ce39ed642eac342db6e9c5a6d3e1092
Ben <jumpeebunee@gmail.com>
2023-05-12 10:47:19 +0300

    feat: add html

--- progress-block/webpack.config.js
+++ progress-block/webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].main.js',
   },
+  mode: 'development',
+  devServer: {
+    port: 4200,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      }
+    }),
+  ]
 };
