b9bf0b2a794ece887b2ebbc88e7d27c06f8a7cad
Ben <jumpeebunee@gmail.com>
2023-05-12 10:56:59 +0300

    feat: add progress

--- progress-block/webpack.config.js
+++ progress-block/webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
   devServer: {
     port: 4200,
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
